6635a59abc5ae1a5ccda4bb181485d9ec224e082
Hyerim <stacyhan0807@gmail.com>
2024-02-11 23:00:43 +0900

    Hello, Stacy 프린트 추가

--- hello.py
+++ hello.py
@@ -1,9 +1,8 @@
 print("hello")
-<<<<<<< HEAD
-print("hello world, goodbye, See you again")
+
+print("hello world, goodbye, See you soon!")
 print("test1, hello!!")
 print("test1, goodbye")
-=======
-print("hello world, goodbye, See you soon!")
 print("test2, Hi!!")
->>>>>>> test2
+
+print("Hello, Stacy")
